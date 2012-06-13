c73741a6db5778a3c2d920c008a3d6eaaf6cfcb5
Sergio <Sergio@hp6730b>
2012-06-13 16:48:03 +0200

    - Ya se env?an y reciben cadenas v?a Bluetooth.

--- app/Bar/Bar/communication/BluetoothServer.cs
+++ app/Bar/Bar/communication/BluetoothServer.cs
@@ -65,13 +65,7 @@ namespace Bar.communication
                     manager = JourneyManager.Instance;
                     string reply = manager.OrdersManager.manageNFCOrder(clientData.Substring(2));
                     StreamWriter sw = new StreamWriter(client.GetStream(), Encoding.ASCII);
-                    //sw.Write(clientData.Substring(2));
-                    //sw.Write("<?xml version=\"1.0\" encoding=\"UTF-8\"?><Bill><Company>MobiCarta</Company><Date>09/06/2012 0:00:00</Date><Table>5</Table><Orders><Order name=\"Macarrones con tomate\" amount=\"2\" price=\"10\" discount=\"0\" total=\"20\"/><Order name=\"Sepia a la plancha\" amount=\"1\" price=\"13,5\" discount=\"0\" total=\"13,5\"/></Orders><PriceSummary subtotal=\"33,5\" discount=\"0\" taxBase=\"33,5\" IVA=\"18\" quote=\"6,03\" total=\"39,53\"/></Bill>");
-                    //sw.Write("<Bill><PriceSummary subtotal=\"33,5\" discount=\"0\" taxBase=\"33,5\" IVA=\"18\" quote=\"6,03\" total=\"39,53\"/></Bill>");
-                    //sw.Write("<?xml version=\"1.0\" encoding=\"UTF-8\"?><Bill><Orders><Order name=\"Macarrones con tomate\" amount=\"2\" price=\"10\" discount=\"0\" total=\"20\"/></Orders></Bill>");
-                    //sw.Write(Convert.ToString("<?xml version=\"1.0\" encoding=\"UTF-8\"?><Bill><Company>MobiCarta</Company><Date>09/06/2012 0:00:00</Date><Table>5</Table><PriceSummary><Subtotal>33</Subtotal><Discount>0</Discount><TaxBase>33</TaxBase><IVA>18</IVA><Quote>6</Quote><Total>39</Total></PriceSummary></Bill>"));
-                    sw.Write(Convert.ToString("<?xml version=\"1.0\" encoding=\"UTF-8\"?><Bill><Company>MobiCarta</Company><Date>09/06/2012 0:00:00</Date><Table>5</Table><Orders><Order><Name>Macarrones con tomate</Name><Amount>2</Amount><Price>10</Price><Discount>0</Discount><Total>20</Total></Order><Order><Name>Sepia a la plancha</Name><Amount>1</Amount><Price>13</Price><Discount>0</Discount><Total>13</Total></Order></Orders><PriceSummary><Subtotal>33</Subtotal><Discount>0</Discount><TaxBase>33</TaxBase><IVA>18</IVA><Quote>6</Quote><Total>39</Total></PriceSummary></Bill>"));
-                    //sw.Write(Convert.ToString(reply));
+                    sw.Write(Convert.ToString(reply));
                     sw.Flush();
                     sw.Close();
                     sr.Close();

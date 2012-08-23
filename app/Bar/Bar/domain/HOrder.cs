fc4ebb8f908794eceb64c27ee5443ee1cea9ccc1
Sergio <Sergio@hp6730b>
2012-08-23 17:16:05 +0200

    - A?adidos archivos.

---
+++ app/Bar/Bar/domain/HOrder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bar.domain
+{
+    class HOrder
+    {
+        private int amount;
+
+        public int Amount
+        {
+            get { return amount; }
+            set { amount = value; }
+        }
+
+        private string client, product;
+
+        public string Client
+        {
+          get { return client; }
+          set { client = value; }
+        }
+
+        public string Product
+        {
+            get { return product; }
+            set { product = value; }
+        }
+
+        private DateTime date;
+
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+
+        public HOrder() { }
+    }
+}
